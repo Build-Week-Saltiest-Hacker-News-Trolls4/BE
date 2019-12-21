672dba9ae7693fe64cdfd4367106aa8d1b90a8f4
Antony Nyagah <antonynyagah@Antonys-MacBook-Pro.local>
2019-12-20 18:09:37 -0600

    register,login and get users routes working, threw in jwt as well

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,5 @@
 const bcrypt = require("bcryptjs");
 const tokenService = require("../auth/token-service.js");
-
 const { authenticate } = require('../auth/authenticate');
 
 const Users = require("./routes-model");
@@ -9,6 +8,8 @@ const Users = require("./routes-model");
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
+  server.get('/api/users',  authenticate, getUsers);
+  server.get('/api/users/:id', authenticate, getUsersbyID)
 };
 
 function register(req, res) {
@@ -47,3 +48,83 @@ function login(req, res) {
       res.status(500).json(error);
     });
 }
+
+function getUsers(req, res) {
+  
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(() => {
+      res.status(500).json({
+        errorMessage: 'The users information could not be retrieved.',
+      });
+    });
+
+
+}
+function getUsersbyID(req,res){
+
+  Users.findById(req.params.id)
+  .then(user => {
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The user with the specified ID does not exist.' });
+    }
+  })
+  .catch(() => {
+    res
+      .status(500)
+      .json({ errorMessage: 'The user information could not be retrieved.' });
+  });
+}
+
+/*
+server.delete('/api/users/:id', (req, res) => {
+  Users.remove(req.params.id)
+    .then(count => {
+      if (count && count > 0) {
+        res.status(200).json({
+          message: 'the user was deleted.',
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: 'The user could not be removed' });
+    });
+});
+
+server.put('/api/users/:id', (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+  } else {
+    Users.update(req.params.id, req.body)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res
+            .status(404)
+            .json({
+              message: 'The user with the specified ID does not exist.',
+            });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage: 'The user information could not be modified.',
+        });
+      });
+  }
+});*/
