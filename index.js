6f7313dfe6609aaf06e3543be20339ac5896fc1c
Antony Nyagah <antonynyagah@Antonys-MacBook-Pro.local>
2019-12-20 18:34:52 -0600

    few config changes so that i can do first deploy

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-require('dotenv').config(); // load .env variables
+// it's recommended to load configuration for .env as early as possible
+require('dotenv').config(); // add this line as the first thing to run1
 
 const server = require('./api/server.js');
 
+// we'll read the port from the server environment if it is there
+// heroku will have the PORT environment variable set
 const port = process.env.PORT || 3300;
+
+// we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
 server.listen(port, () => {
-  console.log(`\n=== Server listening on port ${port}\n`);
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
