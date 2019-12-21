ff53e6ab8e17230d455417bf4dad12b177c4226f
Antony Nyagah <antonynyagah@Antonys-MacBook-Pro.local>
2019-12-20 18:08:29 -0600

    added dotenv to index.js file

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const http = require('http'); // built in node.js module to handle http traffic
-const express = require('express');
+require('dotenv').config(); // load .env variables
 
-const hostname = '127.0.0.1'; // the local computer where the server is running
-const port = 3000; // a port we'll use to watch for traffic
+const server = require('./api/server.js');
 
-
-
-const server = http.createServer((req, res) => {
-  // creates our server
-  res.statusCode = 200; // http status code returned to the client
-  res.setHeader('Content-Type', 'text/plain'); // inform the client that we'll be returning text
-  res.end('Hello from Saltiest Hacker News API! \n'); // end the request and send a response with the specified message
-});
-
-server.listen(port, hostname, () => {
-  // start watching for connections on the port specified
-  console.log(`Server running at http://${hostname}:${port}/`);
+const port = process.env.PORT || 3300;
+server.listen(port, () => {
+  console.log(`\n=== Server listening on port ${port}\n`);
 });
